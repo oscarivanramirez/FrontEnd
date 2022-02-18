9217f08132ca45fb1e63c33d33b5f5f2a837a7f8
oscarivanramirez <oir209@nyu.edu>
2022-02-17 20:29:26 -0500

    can add messages to the chat room on the chat UI

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar(){
     return(
         <div className="Header">
             <div className="logo">
-                <h1>TeleStream</h1>
+                <h1><a href="#">TeleStream</a></h1>
             </div>
             <div className="browse">
                 <input type="text"/>
