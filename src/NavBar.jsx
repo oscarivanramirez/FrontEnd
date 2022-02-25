6feda96c2800102da9019b034dad9e2462cc3209
Lenny <ljr381@nyu.edu>
2022-02-25 16:49:07 -0500

    created pop ups for login/signup and centered the popup to the screen

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,8 +1,24 @@
 import React from "react"
 import './NavBar.css'
 import CreateRoom from "./CreateRoom"
+import Popup from "./PopUp";
+import { useState,useEffect } from 'react';
 
 export default function NavBar(){
+    
+    const [loginState, setLoginState] = useState(false) //to determine if pop up to create a room is showing or not
+    const [signUpState, setSignUpState] = useState(false)
+    
+    const toggleLoginPopUp = () => {
+        setLoginState(!loginState);
+    }
+
+    const toggleSignUpPopUp = () => {
+        setSignUpState(!signUpState);
+    }
+
+
+
     return(
         <div className="Header">
             <div className="logo">
@@ -18,8 +34,35 @@ export default function NavBar(){
                 <nav>
                     <ul>
                         <CreateRoom/>
-                        <li><a href='#'>Log in</a></li>
-                        <li><a href='#'>Sign Up</a></li>
+                        <li><a href='#' onClick={toggleLoginPopUp}>Log in</a>
+                            {loginState && 
+                            <Popup
+                                handleClose = {toggleLoginPopUp}
+                                content = { <>
+                                    <h3>Login</h3>
+                                    <input placeholder = 'Username'></input>
+                                    <br/>
+                                    <input placeholder = 'Password'></input>
+                                    <br/>
+                                    </>
+                                }
+                            />}
+                        </li>
+                        
+                        <li><a href='#' onClick={toggleSignUpPopUp}>Sign Up</a>
+                            {signUpState && 
+                                <Popup
+                                    handleClose = {toggleSignUpPopUp}
+                                    content = { <>
+                                        <h3>Login</h3>
+                                        <input placeholder = 'New Username'></input>
+                                        <br/>
+                                        <input placeholder = 'New Password'></input>
+                                        <br/>
+                                        </>
+                                    }
+                            />}
+                        </li>
 
                     </ul>
                 </nav>
--- src/PopUp.jsx
+++ src/PopUp.jsx
@@ -4,10 +4,12 @@ import './PopUp.css'
 export default function Popup({content, handleClose}){
     return (
         <div className = "modal">
-            <button onClick = {handleClose}>x</button>
-            {content}
-            <button onClick = {handleClose}>Submit</button>
-
+            <div className="modelContent">
+                <button onClick = {handleClose}>x</button>
+                {content}
+                <button onClick = {handleClose}>Submit</button>
+            </div>
+            
         </div>
     );
 };
