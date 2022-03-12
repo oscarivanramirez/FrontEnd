38f8396d6cac4da39448c174f79e3db4d3210eed
Lenny <ljr381@nyu.edu>
2022-03-12 00:48:30 -0500

    Added a slider of content for home screen main content

--- src/Thumbnail.jsx
+++ src/Thumbnail.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import './Thumbnail.css'
+import { Carousel } from "3d-react-carousal";
 
 export default function Thumbnail() {
 
+    let slides = [
+        <ReactPlayer
+            url={"https://youtu.be/hfn0jjy2jI0"}
+            volume={0.00}
+            playing={true}
+        />,
+        <ReactPlayer
+            url={"https://www.youtube.com/watch?v=_ztLXq5Urx8"}
+            volume={0.00}
+            playing={true}
+        />,
+        <ReactPlayer
+            url={"https://youtu.be/ar3oTETtAdE"}
+            volume={0.00}
+            playing={true}
+        />
+    ]
+
   return (
-    <div className="App">
-      <ReactPlayer
-        url={"https://www.youtube.com/watch?v=_ztLXq5Urx8"}
-        volume={0.00}
-        playing={true}
-        config={{
-            youtube: {
-                preload: false,
-            },
-            vimeo: {
-                preload: true
-            },
-        }}
-      />
-    </div>
+    <div>
+        <Carousel slides={slides}/>
+    </div>  
+   
   );
 }
