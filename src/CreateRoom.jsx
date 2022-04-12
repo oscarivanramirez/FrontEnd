a81cb4de87bef8889207ce185b16f6cb94263948
jp5169 <jp5169@nyu.edu>
2022-04-12 15:16:04 -0400

    Added conditional rendering to CreateRoom.jsx fixed the original problem of having to login first and logging out to not see the create room button
    Co-authored-by: oscarivanramirez <oir209@nyu.edu>

--- src/CreateRoom.jsx
+++ src/CreateRoom.jsx
@@ -37,45 +37,47 @@ export default function CreateRoom(){
     
 
     return(
-        <div>
-            {localStorage.getItem('SessionInfo') === 'Not signed in yet' &&
-                <a href="#" className="roomButton" onClick={togglePopUp}>
-                    Create Room
-                </a>
-            }
-            {state &&
-            <Popup
-                handleCloseX = {togglePopUp}
-                content = { <>
-                    <h3>Create Your New Room</h3>
-                    <input 
-                        value={roomName}
-                        onChange={(event) => setRoomName(event.target.value)}
-                        placeholder = 'Enter Your Room Name'></input>
-                    <br/>
+        <>
+            {
+                session.name.userName ? (
+                    <>
+                        <a href="#" className="roomButton" onClick={togglePopUp}>Create Room</a>
+                        {state &&
+                            <Popup
+                                handleCloseX = {togglePopUp}
+                                content = { 
+                                    <>
+                                        <h3>Create Your New Room</h3>
+                                        <input 
+                                            value={roomName}
+                                            onChange={(event) => setRoomName(event.target.value)}
+                                            placeholder = 'Enter Your Room Name'></input>
+                                        <br/>
 
-                    <label for="genreSelect">Genres</label>
-                    <br/>
-                    <select id="genreSelect" onChange={(event) => setGenre(event.target.value)}>
-                        <option value="NULL">Select</option>
-                        <option value="Games">Games</option>
-                        <option value="IRL">IRL</option>
-                        <option value="Music">Music</option>
-                        <option value="Esports">Esports</option>
-                        <option value="Creative">Creative</option>
-                        
-                    </select>
-                    <br/>
-
-                    {/*<input 
-                        value={genre}
-                        onChange={(event) => setGenre(event.target.value)}
-                        placeholder = 'Enter the genre'></input>
-                    <br/>*/}
+                                        <label for="genreSelect">Genres</label>
+                                        <br/>
+                                        <select id="genreSelect" onChange={(event) => setGenre(event.target.value)}>
+                                            <option value="NULL">Select</option>
+                                            <option value="Games">Games</option>
+                                            <option value="IRL">IRL</option>
+                                            <option value="Music">Music</option>
+                                            <option value="Esports">Esports</option>
+                                            <option value="Creative">Creative</option>
+                                            
+                                        </select>
+                                        <br/>
+                                    </>
+                                }
+                                handleCloseS = {handleCreateRoom}
+                            />
+                        }    
                     </>
-                }
-                handleCloseS = {handleCreateRoom}
-            />}
-        </div>
+                ):(
+                    <>
+                    </>
+                )
+            }
+        </>
+
     )
 }
