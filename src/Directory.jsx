44e969515a2707caf90b3952e7bee1e168478f9e
oscarivanramirez <oir209@nyu.edu>
2022-04-13 20:44:59 -0400

    made a directory url for all the genres & lists all the chat rooms in that genre

--- src/Directory.jsx
+++ src/Directory.jsx
@@ -1,27 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Nav from "./NavBar"
 import { Carousel } from "3d-react-carousal";
 import ReactPlayer from "react-player";
 import Genre from './Genre';
 import HomeScreen from './Links/HomeScreen';
-import {useParams} from 'react-router-dom';
+import {Link,useParams} from 'react-router-dom';
+import AR from "./ActiveRoom"
 import './Directory.css'
 
 export default function Directory(){
 
+    const GENRES = ['Games','IRL','Music','Esports','Creative']
     let {genre} = useParams();
+    const [error, setError] = useState('');
+    const [rooms, setRooms] = useState(undefined);
+    const [currgenre, setGenre] = useState(undefined);
+    setGenre({genre});
 
     let slides = [
-        <div>
-            
-        </div>
+        <div> </div>
     ]
+    useEffect(() => {
+        axios.get(`https://swejol.herokuapp.com/rooms/list/${currgenre}`)
+        .then((res) => {
+            if(res.data){
+                setRooms(res.data);
+                console.log(rooms)
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            setError(err.toString());
+        })
+    }, [currgenre])
     
+    const PrevGenre = () => {
+
+    }
+    const NextGenre = () => {
+        
+    }
+
+
     return(
         <div className="genre-animated-background">
             <Nav/>
-            <h1 class='genreName'>{genre}</h1>
-            <Carousel slides={slides} arrows={true}/>
+            <button onClick={PrevGenre}>Left</button>
+            <h1>{genre}</h1>
+            <button onClick={NextGenre}>Right</button>
+            <div class="genresCarousel">
+                {
+                    rooms && rooms.map((room, index) => (
+                        <Link to={`/ChatUI/${room.roomName}`}>
+                            <div className={`gc${index+1}`}>
+                                <AR                                key={`${room}`}
+                                    roomStreamer={room.createrName}
+                                    roomName={room.roomName}
+                                    numUsers={room.num_users}
+                                    
+                                />
+                                {/*<Link to={"/ChatUI"}></Link> */}
+                                
+                                
+                            </div>
+                        </Link>
+                        
+                    ))
+
+
+                }
+                {/*
+                <div class="gc1"></div>
+                <div class="gc2"></div>
+                <div class="gc3"></div>
+                <div class="gc4"></div>
+                <div class="gc5"></div>
+                <div class="gc6"></div>
+                <div class="gc7"></div>
+                <div class="gc8"></div>
+                <div class="gc9"></div>
+                <div class="gc10"></div>
+                <div class="gc11"></div>
+                <div class="gc12"></div>
+                */}
+            </div>
         </div>
     )
 }
